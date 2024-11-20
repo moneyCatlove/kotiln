147df91d6def8052dc1376909be4bbc56a5a4b53
Jihyuk Lee <lead@sver.dev>
2024-11-20 21:06:58 +0900

    typo :: 클래스명 오타 수정

--- app/src/main/java/com/example/smartring/model/CurrectHealthModel.kt
+++ app/src/main/java/com/example/smartring/model/CurrectHealthModel.kt
@@ -1,6 +1,6 @@
 package com.example.smartring.model
 
-data class CurrectHealthModel(
+data class CurrentHealthModel(
     val heart_rate: Int, // 현재 심박수
     val sleep_state: Int, // 현재 수면 상태 (0=깨어있음, 1=얕은 수면, 2=깊은 수면)
     val step_count: Int, // 현재 걸음 수
