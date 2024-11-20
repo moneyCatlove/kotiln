deddf23052299460186962eac7cfc42074bd6bde
Jihyuk Lee <lead@sver.dev>
2024-11-20 18:01:30 +0900

    add :: 타입 추가

---
+++ app/src/main/java/com/example/smartring/util/JsonInfo.kt
@@ -0,0 +1,9 @@
+package com.example.smartring.util
+
+import org.json.JSONObject
+import java.io.Serializable
+
+class JsonInfo : Serializable {
+    var cmdKey: String? = null
+    var jsonObject: JSONObject? = null
+}
